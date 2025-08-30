a81e2f83d977dfbae2f2d8cdd28632da75b72404
nikita <niki2ta00n4@gmail.com>
2025-08-30 16:05:18 +0300

    add prisma scheme

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,9 +1,10 @@
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import { NextResponse } from 'next/server';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+console.log(process.env.POSTGRES_URL)
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
@@ -110,8 +111,8 @@ export async function GET() {
       seedRevenue(),
     ]);
 
-    return Response.json({ message: 'Database seeded successfully' });
+    return NextResponse.json({ message: 'Database seeded successfully' });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
